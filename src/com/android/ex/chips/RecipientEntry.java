f7dcdec98d429889bcebc5e3ff35406aed8f0b4a
Danny Baumann <dannybaumann@web.de>
2014-12-03 10:21:09 +0100

    Always trim destination.
    Makes sure isPhoneNumber() recognizes phone numbers even if they are
    postfixed with a space.
    
    Change-Id: I9a26488b5286c6b6e936e09403396a225ee3b8d5

--- src/com/android/ex/chips/RecipientEntry.java
+++ src/com/android/ex/chips/RecipientEntry.java
@@ -271,7 +271,7 @@ public class RecipientEntry {
     }
 
     public String getDestination() {
-        return mDestination;
+        return mDestination != null ? mDestination.trim() : null;
     }
 
     public int getDestinationType() {
